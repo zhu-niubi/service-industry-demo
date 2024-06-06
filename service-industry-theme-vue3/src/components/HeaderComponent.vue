bfba8821fc5a2e0a314a5f7d694cce425149a0ca
zhulanjin <2841816585@qq.com>
2024-06-06 17:40:55 +0800

    1

---
+++ service-industry-theme-vue3/src/components/HeaderComponent.vue
@@ -0,0 +1,424 @@
+<template>
+  <header class="header-style1 menu_area-light">
+    <div class="navbar-default border-bottom border-color-light-white">
+      <!-- start top search -->
+      <div class="top-search bg-primary">
+        <div class="container-fluid px-lg-1-6 px-xl-2-5 px-xxl-2-9">
+          <form
+            class="search-form"
+            action="search.html"
+            method="GET"
+            accept-charset="utf-8"
+          >
+            <div class="input-group">
+              <span class="input-group-addon cursor-pointer">
+                <button
+                  class="search-form_submit fas fa-search text-white"
+                  type="submit"
+                ></button>
+              </span>
+              <input
+                type="text"
+                class="search-form_input form-control"
+                name="s"
+                autocomplete="off"
+                placeholder="Type & hit enter..."
+              />
+              <span class="input-group-addon close-search mt-1"
+                ><i class="fas fa-times"></i
+              ></span>
+            </div>
+          </form>
+        </div>
+      </div>
+      <!-- end top search -->
+      <div class="container-fluid px-lg-1-6 px-xl-2-5 px-xxl-2-9">
+        <div class="row align-items-center">
+          <div class="col-12">
+            <div class="menu_area alt-font">
+              <nav class="navbar navbar-expand-lg navbar-light p-0">
+                <div class="navbar-header navbar-header-custom">
+                  <!-- start logo -->
+                  <a href="index.html" class="navbar-brand h-default"
+                    ><img id="logo" src="/images/logos/logo-inner.png" alt="logo"
+                  /></a>
+                  <!-- end logo -->
+                </div>
+
+                <div class="navbar-toggler"></div>
+
+                <!-- menu area -->
+                <ul class="navbar-nav ms-auto" id="nav" style="display: none">
+                  <li>
+                    <a href="#!" >Home</a>
+                    <ul>
+                      <li><a href="index-movers.html">Moving Company</a></li>
+                      <li><a href="index-gardener.html">Gardener</a></li>
+                      <li><a href="index-carwash.html">Car Wash</a></li>
+                      <li><a href="index-roofing.html">Roofing</a></li>
+                      <li><a href="index-laundry.html">Laundry</a></li>
+                      <li><a href="index-decorator.html">Decorator</a></li>
+                      <li><a href="index-courier.html">Courier</a></li>
+                      <li><a href="index-tech-repair.html">Tech Repair</a></li>
+                      <li><a href="index-electrician.html">Electrician</a></li>
+                      <li><a href="index-plumber.html">Plumber</a></li>
+                      <li><a href="index-mechanic.html">Mechanic</a></li>
+                      <li><a href="index-cleaning.html">Cleaning</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="#!">Pages</a>
+                    <ul>
+                      <li><a href="aboutus.html">About Us</a></li>
+                      <li>
+                        <a href="company-history.html">Company History</a>
+                      </li>
+                      <li><a href="our-process.html">Our Process</a></li>
+                      <li><a href="our-clients.html">Our Clients</a></li>
+                      <li><a href="contactus.html">Contact Us</a></li>
+                      <li>
+                        <a href="#!">Services</a>
+                        <ul>
+                          <li><a href="services.html">Services</a></li>
+                          <li>
+                            <a href="service-details.html">Service Details</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <a href="#!">Team</a>
+                        <ul>
+                          <li><a href="our-team.html">Our Team</a></li>
+                          <li><a href="team-detail.html">Team Detail</a></li>
+                        </ul>
+                      </li>
+                      <li>
+                        <a href="#!">Careers</a>
+                        <ul>
+                          <li><a href="career.html">Career</a></li>
+                          <li>
+                            <a href="career-detail.html">Career Detail</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <a href="#!">Additional Pages</a>
+                        <ul>
+                          <li><a href="testimonials.html">Testimonials</a></li>
+                          <li>
+                            <a href="pricing-plans.html">Pricing Plans</a>
+                          </li>
+                          <li><a href="offers.html">Offers</a></li>
+                          <li><a href="faq.html">FAQ</a></li>
+                        </ul>
+                      </li>
+                      <li>
+                        <a href="#!">Others</a>
+                        <ul>
+                          <li>
+                            <a href="privacy-policy.html">Privacy Policy</a>
+                          </li>
+                          <li>
+                            <a href="terms-and-conditions.html"
+                              >Terms and Conditions</a
+                            >
+                          </li>
+                          <li><a href="maintenance.html">Maintenance</a></li>
+                          <li><a href="comingsoon.html">Coming Soon</a></li>
+                          <li><a href="404-error.html">404 Error</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="#!">Features</a>
+                    <ul>
+                      <li>
+                        <a href="#!">Headers</a>
+                        <ul>
+                          <li><a href="header-1.html">Header Style 1</a></li>
+                          <li><a href="header-2.html">Header Style 2</a></li>
+                          <li><a href="header-3.html">Header Style 3</a></li>
+                          <li><a href="header-4.html">Header Style 4</a></li>
+                        </ul>
+                      </li>
+                      <li>
+                        <a href="#!">Footers</a>
+                        <ul>
+                          <li><a href="footer-1.html">Footer Style 1</a></li>
+                          <li><a href="footer-2.html">Footer Style 2</a></li>
+                          <li><a href="footer-3.html">Footer Style 3</a></li>
+                          <li><a href="footer-4.html">Footer Style 4</a></li>
+                          <li><a href="footer-5.html">Footer Style 5</a></li>
+                          <li><a href="footer-6.html">Footer Style 6</a></li>
+                          <li><a href="footer-7.html">Footer Style 7</a></li>
+                          <li><a href="footer-8.html">Footer Style 8</a></li>
+                          <li><a href="footer-9.html">Footer Style 9</a></li>
+                          <li><a href="footer-10.html">Footer Style 10</a></li>
+                          <li><a href="footer-11.html">Footer Style 11</a></li>
+                          <li><a href="footer-12.html">Footer Style 12</a></li>
+                        </ul>
+                      </li>
+                      <li>
+                        <a href="#!">Owl Sliders</a>
+                        <ul>
+                          <li><a href="owl-slider1.html">Owl Slider 1</a></li>
+                          <li><a href="owl-slider2.html">Owl Slider 2</a></li>
+                          <li><a href="owl-slider3.html">Owl Slider 3</a></li>
+                          <li><a href="owl-slider4.html">Owl Slider 4</a></li>
+                          <li><a href="owl-slider5.html">Owl Slider 5</a></li>
+                        </ul>
+                      </li>
+                      <li>
+                        <a href="#!">Revolution Sliders</a>
+                        <ul>
+                          <li>
+                            <a href="rs-slider1.html">Revolution Slider 1</a>
+                          </li>
+                          <li>
+                            <a href="rs-slider2.html">Revolution Slider 2</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <a href="creative-banner.html">Creative Banner</a>
+                      </li>
+                      <li>
+                        <a href="animated-banner.html">Animated Banner</a>
+                      </li>
+                      <li>
+                        <a href="parallax-banner.html">Parallax Banner</a>
+                      </li>
+                      <li>
+                        <a href="video-banner.html">Video Banner</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="#!">Projects</a>
+                    <ul>
+                      <li>
+                        <a href="project-3-column.html">Project 3 Column</a>
+                      </li>
+                      <li>
+                        <a href="project-4-column.html">Project 4 Column</a>
+                      </li>
+                      <li>
+                        <a href="project-details.html">Project Details</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="#!">Blog</a>
+                    <ul>
+                      <li><a href="blog-grid.html">Blog Grid</a></li>
+                      <li><a href="blog-list.html">Blog List</a></li>
+                      <li><a href="blog-detail.html">Blog Detail</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="#!">Shop</a>
+                    <ul>
+                      <li><a href="product-grid.html">Product Grid</a></li>
+                      <li><a href="product-detail.html">Product Detail</a></li>
+                      <li><a href="cart.html">Cart</a></li>
+                      <li><a href="checkout.html">Checkout</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="#!">Elements</a>
+                    <ul class="row megamenu">
+                      <li class="col-lg-3">
+                        <span
+                          class="mb-0 mb-lg-3 d-block py-2 p-lg-0 px-4 px-lg-0 text-uppercase sub-title font-weight-600 display-30"
+                          >Elements 01</span
+                        >
+                        <ul>
+                          <li>
+                            <a href="accordion.html"
+                              ><i class="fas fa-sliders-h me-2"></i
+                              >Accordions</a
+                            >
+                          </li>
+                          <li>
+                            <a href="alerts.html"
+                              ><i class="far fa-bell me-2"></i>Alerts</a
+                            >
+                          </li>
+                          <li>
+                            <a href="blockquotes.html"
+                              ><i class="fas fa-vector-square me-2"></i
+                              >Blockquote</a
+                            >
+                          </li>
+                          <li>
+                            <a href="buttons.html"
+                              ><i class="fas fa-link me-2"></i>Buttons</a
+                            >
+                          </li>
+                          <li>
+                            <a href="call-to-action.html"
+                              ><i class="far fa-square me-2"></i>Call to
+                              Action</a
+                            >
+                          </li>
+                          <li>
+                            <a href="carousel-slider.html"
+                              ><i class="far fa-images me-2"></i>Carousel
+                              Slider</a
+                            >
+                          </li>
+                        </ul>
+                      </li>
+                      <li class="col-lg-3">
+                        <span
+                          class="mb-0 mb-lg-3 d-block py-2 p-lg-0 px-4 px-lg-0 text-uppercase sub-title font-weight-600 display-30"
+                          >Elements 02</span
+                        >
+                        <ul>
+                          <li>
+                            <a href="count-down.html"
+                              ><i class="far fa-flag me-2"></i>Count Down</a
+                            >
+                          </li>
+                          <li>
+                            <a href="counter.html"
+                              ><i class="fas fa-compress me-2"></i>Counters</a
+                            >
+                          </li>
+                          <li>
+                            <a href="forms.html"
+                              ><i class="fab fa-wpforms me-2"></i>Forms</a
+                            >
+                          </li>
+                          <li>
+                            <a href="font-icons.html"
+                              ><i class="far fa-check-square me-2"></i>Font
+                              Icons</a
+                            >
+                          </li>
+                          <li>
+                            <a href="google-map.html"
+                              ><i class="fas fa-map-marker-alt me-2"></i>Google
+                              Map</a
+                            >
+                          </li>
+                          <li>
+                            <a href="grid-system.html"
+                              ><i class="fas fa-th me-2"></i>Grid System</a
+                            >
+                          </li>
+                        </ul>
+                      </li>
+                      <li class="col-lg-3">
+                        <span
+                          class="mb-0 mb-lg-3 d-block py-2 p-lg-0 px-4 px-lg-0 text-uppercase sub-title font-weight-600 display-30"
+                          >Elements 03</span
+                        >
+                        <ul>
+                          <li>
+                            <a href="icon-with-text.html"
+                              ><i class="fab fa-fonticons-fi me-2"></i>Icon With
+                              Text</a
+                            >
+                          </li>
+                          <li>
+                            <a href="list-styles.html"
+                              ><i class="fas fa-list-ul me-2"></i>List Styles</a
+                            >
+                          </li>
+                          <li>
+                            <a href="media-object.html"
+                              ><i class="fas fa-photo-video me-2"></i>Media
+                              Object</a
+                            >
+                          </li>
+                          <li>
+                            <a href="modal.html"
+                              ><i class="fas fa-expand me-2"></i>Modal</a
+                            >
+                          </li>
+                          <li>
+                            <a href="pagination.html"
+                              ><i class="far fa-caret-square-right me-2"></i
+                              >Pagination</a
+                            >
+                          </li>
+                          <li>
+                            <a href="progress-bar.html"
+                              ><i class="fas fa-tasks me-2"></i>Progress Bars</a
+                            >
+                          </li>
+                        </ul>
+                      </li>
+                      <li class="col-lg-3">
+                        <span
+                          class="mb-0 mb-lg-3 d-block py-2 p-lg-0 px-4 px-lg-0 text-uppercase sub-title font-weight-600 display-30"
+                          >Elements 04</span
+                        >
+                        <ul>
+                          <li>
+                            <a href="page-title1.html"
+                              ><i class="fas fa-compress me-2"></i>Page
+                              Title1</a
+                            >
+                          </li>
+                          <li>
+                            <a href="page-title2.html"
+                              ><i class="fas fa-compress-arrows-alt me-2"></i
+                              >Page Title2</a
+                            >
+                          </li>
+                          <li>
+                            <a href="tables.html"
+                              ><i class="fas fa-table me-2"></i>Tables</a
+                            >
+                          </li>
+                          <li>
+                            <a href="tabs.html"
+                              ><i class="fas fa-sliders-h me-2"></i>Tabs</a
+                            >
+                          </li>
+                          <li>
+                            <a href="typography.html"
+                              ><i class="fas fa-text-height me-2"></i
+                              >Typography</a
+                            >
+                          </li>
+                          <li>
+                            <a href="video.html"
+                              ><i class="fas fa-video me-2"></i>Videos</a
+                            >
+                          </li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+                <!-- end menu area -->
+
+                <!-- start attribute navigation -->
+                <div
+                  class="attr-nav align-items-lg-center ms-lg-auto main-font"
+                >
+                  <ul>
+                    <li class="search">
+                      <a href="#!"><i class="fas fa-search"></i></a>
+                    </li>
+                    <li class="d-none d-xl-inline-block">
+                      <a href="contactus.html" class="butn medium"
+                        ><span>Free Quote</span></a
+                      >
+                    </li>
+                  </ul>
+                </div>
+                <!-- end attribute navigation -->
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
---
+++ service-industry-theme-vue3/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="preloader"></div>
+  <div class="main-wrapper">
+    <HeaderComponent />
+    <IndexComponent />
+    <FooterComponent />
+  </div>
+  <a href="#!" class="scroll-to-top"><i class="fas fa-angle-up" aria-hidden="true"></i></a>
+</template>
+
+<script>
+import $ from "jquery";
+import IndexComponent from '@/components/IndexComponent.vue'
+import HeaderComponent from '@/components/HeaderComponent.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
+
+
+export default {
+  name: "HomeView",
+  components: {
+    IndexComponent, HeaderComponent, FooterComponent
+  },
+  mounted() {
+    $(document).ready(function () {
+    });
+  },
+};
+</script>
